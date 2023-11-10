667d825b6812a55339b81bcd6807e59bc33000f0
David Wardlaw <DWardlaw@imeta.co.uk>
2023-11-10 10:44:52 +0000

    Addition of assertions

--- PlaywrightUITesting/PlaywrightUITesting/Pages/MainHeader.cs
+++ PlaywrightUITesting/PlaywrightUITesting/Pages/MainHeader.cs
@@ -14,30 +14,35 @@ namespace PlaywrightUITesting.Pages
             private readonly ILocator _logo;
             private readonly ILocator _categoryDropdown;
             private readonly ILocator _search;
-            private readonly ILocator _searchButton;
             private readonly ILocator _compare;
             private readonly ILocator _like;
+            private readonly ILocator _cart;
 
-            public MainHeader(IPage page)
+        /*test('test', async ({ page }) => {
+            await page.goto('https://ecommerce-playground.lambdatest.io/');
+            await page.getByRole('link', { name: 'Poco Electro' }).click();
+            await page.getByLabel('Compare').click();
+            await page.getByLabel('Wishlist').click();
+            await page.getByRole('button', { name: '0' }).click();
+            await page.getByRole('button', { name: '0' }).click();
+        }); */
+
+        public MainHeader(IPage page)
             {
                 _page = page;
-                _logo = _page.GetByLabel("Poco Electro");
-                _categoryDropdown = _page.GetByLabel("All Categories");
-                _search = _page.GetByLabel("Search for Products");
-                _searchButton = _page.GetByRole(AriaRole.Button, new() { Name = "Search" });
-                _compare = _page.GetByTitle("Compare");
-                _like= _page.GetByTitle("Wishlist");
+                _logo = _page.GetByRole(AriaRole.Link, new() { Name = "Poco Electro" });
+                _compare  = _page.GetByLabel("Compare");
+                _like = _page.GetByLabel("WishList");
+                _cart = _page.GetByRole(AriaRole.Button, new() { Name = "0" });
             }
 
-            public List<ILocator> CheckElementsAreAvailable()
+            public Dictionary<string, ILocator> AddElementsAreAvailableToBeClicked()
             {
-                List<ILocator> items = new List<ILocator>();
-                items.Add(_logo);
-                items.Add(_categoryDropdown);
-                items.Add(_search);
-                items.Add(_searchButton);
-                items.Add(_compare);
-                items.Add(_like);
+                Dictionary<string, ILocator> items = new Dictionary<string, ILocator>();
+                items.Add("logo", _logo);
+                items.Add("compare", _compare);
+                items.Add("like", _like);
+                items.Add("cart",_cart);
 
                 return items;
             }
--- PlaywrightUITesting/PlaywrightUITesting/Tests/MainHeaderTests.cs
+++ PlaywrightUITesting/PlaywrightUITesting/Tests/MainHeaderTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 using PlaywrightUITesting.Pages;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PlaywrightUITesting.Tests
 {
@@ -22,20 +24,44 @@ namespace PlaywrightUITesting.Tests
         }
 
         [Test]
-        public async Task CheckMainHeader()
+        public async Task CheckMainHeaderNonSearchElements()
         {
             await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
-                Headless = false
+               Headless = false
             });
 
             var page = await browser.NewPageAsync();
 
-            await Page.GotoAsync("https://ecommerce-playground.lambdatest.io/");
+            await page.GotoAsync("https://ecommerce-playground.lambdatest.io/");
 
             MainHeader mh = new MainHeader(page);
-            Assert.AreEqual(6, mh.CheckElementsAreAvailable().Count);
+            var nonSearchElements = mh.AddElementsAreAvailableToBeClicked();
 
+            foreach (var item in nonSearchElements)
+            {
+                await item.Value.ClickAsync();
+
+                if (item.Key == "logo")
+                {
+                    await Expect(page)
+                        .ToHaveURLAsync("https://ecommerce-playground.lambdatest.io/index.php?route=common/home");
+                }
+                if (item.Key == "compare")
+                {
+                    await Expect(page)
+                        .ToHaveURLAsync("https://ecommerce-playground.lambdatest.io/index.php?route=product/compare");
+                }
+                if (item.Key == "like")
+                {
+                    await Expect(page)
+                        .ToHaveURLAsync("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
+                }
+                if (item.Key == "cart")
+                {
+                    await Expect(page.GetByText("Your shopping is empty")).ToBeAttachedAsync();
+                }
+            } 
         }
     }
 }
