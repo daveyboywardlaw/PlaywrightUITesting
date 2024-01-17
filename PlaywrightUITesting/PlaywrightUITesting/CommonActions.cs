c07373d839e34206437d3c3ed4ff696e55407bce
David Wardlaw <DWardlaw@imeta.co.uk>
2024-01-17 13:27:04 +0000

    Add basic search test

---
+++ PlaywrightUITesting/PlaywrightUITesting/CommonActions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightUITesting
+{
+    public class CommonActions : PageTest
+    {
+        public void ClickButton(IPage page,  string buttonText)
+        {
+            var button =  page.GetByRole(AriaRole.Button, new() { Name = buttonText }).ClickAsync();
+        }
+
+        public ILocator FindButton(IPage page, string buttonText)
+        {
+            return page.GetByRole(AriaRole.Button, new() { Name = buttonText });
+        }
+
+        public void SelectCategory(string category)
+        {
+            // Method to select a dropdown value
+        }
+
+        public void PopulateField(ILocator locator, string value)
+        {
+            locator.FillAsync(value);
+        }
+
+    }
+}
---
+++ PlaywrightUITesting/PlaywrightUITesting/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightUITesting
+{
+    public class Utilities
+    {
+        public async Task<IPage> CreateNewPage()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Webkit.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            return page;
+        }
+    }
+}
