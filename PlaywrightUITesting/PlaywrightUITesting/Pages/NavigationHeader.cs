6a0a7413e61d3c66042410a8fe81cdbf0a4a3e65
David Wardlaw <DWardlaw@imeta.co.uk>
2024-02-18 18:16:51 +0000

    Updates find navigation header links

---
+++ PlaywrightUITesting/PlaywrightUITesting/Pages/NavigationHeader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightUITesting.Pages
+{
+    internal class NavigationHeader : PageTest
+    {
+        public ILocator ShopByCategory;
+        public ILocator Home;
+        public ILocator Special;
+        public ILocator Blog;
+        public ILocator MegaMenu;
+        public ILocator AddOns;
+        public ILocator MyAccount;
+        public NavigationHeader()
+        {
+            ShopByCategory = Page.GetByLabel("Shop by Category");
+            Home = Page.GetByText("Home");
+            Special = Page.GetByText("Special");
+            Blog = Page.GetByText("Blog");
+            MegaMenu = Page.GetByText("Mega Menu");
+            AddOns = Page.GetByText("AddOns");
+            MyAccount = Page.GetByText("My Account");
+        }
+
+        public Dictionary<string, ILocator> AddElementsAreAvailableToBeClicked()
+        {
+            Dictionary<string, ILocator> items = new Dictionary<string, ILocator>();
+            items.Add("Shop By Category", ShopByCategory);
+            items.Add("Home", Home);
+            items.Add("Special", Special);
+            items.Add("Blog", Blog);
+            items.Add("Mega Menu", MegaMenu);
+            items.Add("AddOns", AddOns);
+            items.Add("My Account", MyAccount);
+
+            return items;
+        }
+    }
+}
---
+++ PlaywrightUITesting/PlaywrightUITesting/Tests/NavigationHeaderTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightUITesting.Tests
+{
+    internal class NavigationHeaderTests : PageTest
+    {
+        CommonActions actions = new CommonActions();
+        private static string url;
+
+        [SetUp]
+        public void SetupAsync()
+        {
+            url = TestContext.Parameters["WebAppUrl"]
+                  ?? throw new Exception("Web url not configured");
+        }
+
+        [Test]
+        public async Task SearchAllCategories()
+        {
+            await Page.GotoAsync(url);
+            actions.ClickButton(Page, "Search");
+
+            //Assertion
+            await Expect(Page)
+                .ToHaveURLAsync("https://ecommerce-playground.lambdatest.io/index.php?route=product%2Fsearch&search=");
+        }
+    }
+}
